a0366292eccb143ccb19e3b315dba7d5065f7b6a
trinh.thanh.do@use-inc.co.jp <trinh.thanh.do@use-inc.co.jp>
2023-12-21 18:30:49 +0900

    add framework project

---
+++ CCanPassThru/CCanPassThru/CCanPassThru.h
@@ -0,0 +1,18 @@
+//
+//  CCanPassThru.h
+//  CCanPassThru
+//
+//  Created by dotrinh on 2023/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CCanPassThru.
+FOUNDATION_EXPORT double CCanPassThruVersionNumber;
+
+//! Project version string for CCanPassThru.
+FOUNDATION_EXPORT const unsigned char CCanPassThruVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CCanPassThru/PublicHeader.h>
+
+
